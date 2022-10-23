ea03475c7beec14fbab9ecadf2b8a5e51ab68ee0
SamvsP4K <soundsofsamvs@gmail.com>
2022-10-22 18:48:08 -0700

    intial push of required files

---
+++ app.py
@@ -0,0 +1,77 @@
+#Importing Despendancies
+import datetime as dt
+from lib2to3.pgen2.token import STRING
+from tkinter.tix import INTEGER
+import numpy  as np
+import pandas as pd
+
+#SQLAlchemy dependancies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from zmq import MECHANISM
+
+from flask import Flask, jsonify
+
+#Creating connection to database for reflection and query
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine,reflect=True)
+
+#Saving table references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Session link
+session = Session(engine)
+
+#Flask App creation Start
+app = Flask(__name__)
+
+#define welcome route
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api.v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+#precipitation route
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date,Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+#Station route
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations = stations)
+
+#Temperature observation routes
+@app.route("/api/v1.0/tobs")
+
+def  temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
